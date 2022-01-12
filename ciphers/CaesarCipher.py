7b58e05bba2f7abca1732ffdfdea5629b80523f7
qpwoeirut <stanleyzhong8@gmail.com>
2022-01-12 15:58:36 -0800

    Add a test for Caesar Cipher from Wikipedia

--- ciphers/CaesarCipher.py
+++ ciphers/CaesarCipher.py
@@ -7,3 +7,20 @@ from ciphers.AffineCipher import AffineCipher
 class CaesarCipher(AffineCipher):
     def __init__(self, rotations: int, alphabet=string.ascii_lowercase):
         super().__init__(a=1, b=rotations, alphabet=alphabet)
+
+
+def test():
+    # test from Wikipedia
+    cipher = CaesarCipher(23)
+    pt = cipher.filter_invalid("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG")
+    ct = cipher.filter_invalid("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD")
+    assert cipher.encrypt(pt) == ct
+    assert cipher.decrypt(ct) == pt
+
+
+def main():
+    test()
+
+
+if __name__ == '__main__':
+    main()
