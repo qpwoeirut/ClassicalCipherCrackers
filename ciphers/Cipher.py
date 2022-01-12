8f1c71a4fa1d363340a00e6dfeae9d6a28aa5cf5
qpwoeirut <stanleyzhong8@gmail.com>
2022-01-12 15:51:39 -0800

    Add abstract Cipher class

---
+++ ciphers/Cipher.py
@@ -0,0 +1,33 @@
+import abc
+import string
+
+
+class Cipher(metaclass=abc.ABCMeta):
+    def __init__(self, key, alphabet=string.ascii_lowercase):
+        self.key = key
+        self.alphabet = alphabet
+
+    def filter_invalid(self, text: str) -> str:
+        return ''.join([c.lower() for c in text if self.valid_char(c.lower())])
+
+    def valid_text(self, text: str) -> bool:
+        return len(self.filter_invalid(text)) == len(text)
+
+    def valid_char(self, c: str) -> bool:
+        return c in self.alphabet
+
+    @abc.abstractmethod
+    def encrypt(self, plaintext: str) -> str:
+        """
+        Encrypts plaintext using the implemented cipher
+        Assumes plaintext is valid (all chars are in self.alphabet)
+        """
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def decrypt(self, ciphertext: str) -> str:
+        """
+        Decrypts ciphertext using the implemented cipher
+        Assumes ciphertext is valid (all chars are in self.alphabet)
+        """
+        raise NotImplementedError()
