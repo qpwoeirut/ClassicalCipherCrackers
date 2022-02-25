3e80e38c025bd687380a6e26caf0374c8a9dc8b0
qpwoeirut <stanleyzhong8@gmail.com>
2022-02-24 17:42:34 -0800

    Don't store transformed_alphabet in the SubstitutionCipher class

--- ciphers/SubstitutionCipher.py
+++ ciphers/SubstitutionCipher.py
@@ -7,9 +7,8 @@ class SubstitutionCipher(Cipher):
     def __init__(self, transformed_alphabet: str, alphabet: str = string.ascii_uppercase):
         super().__init__(alphabet=alphabet)
 
-        self.transformed_alphabet = transformed_alphabet
-        self.encryption_table = str.maketrans(self.alphabet, self.transformed_alphabet)
-        self.decryption_table = str.maketrans(self.transformed_alphabet, self.alphabet)
+        self.encryption_table = str.maketrans(self.alphabet, transformed_alphabet)
+        self.decryption_table = str.maketrans(transformed_alphabet, self.alphabet)
 
     def encrypt(self, plaintext: str) -> str:
         return plaintext.translate(self.encryption_table)
