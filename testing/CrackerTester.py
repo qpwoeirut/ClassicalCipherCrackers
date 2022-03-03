da2bd2522b719d5e91ecca09442494240e3b1beb
qpwoeirut <stanleyzhong8@gmail.com>
2022-03-03 15:20:50 -0800

    Add on stuff to CrackerTester, rename timer.py to CrackerTester.py

--- testing/CrackerTester.py
+++ testing/CrackerTester.py
@@ -7,20 +7,18 @@ from crackers.VigenereCracker import VigenereCracker
 random.seed(0)  # seed to ensure easy reproducibility
 
 
-class CrackerTimer:
+class CrackerTester:
     def __init__(self, text: str, cracker: Cracker):
-        self.text = text.strip()
         self.cracker = cracker
-        self.start_points = [0] + [i+1 for i in range(len(self.text)) if self.text[i].isspace()] + [len(self.text)]
-        if len(self.start_points) < 3:
-            self.start_points = range(len(self.text))
+        self.text = self.cracker.cipher("", alphabet=cracker.alphabet).filter_invalid(text.strip())
 
     def pick_random_excerpt(self):
-        start = random.choice(self.start_points)
-        finish = random.choice(self.start_points)
-        while start == finish:
-            finish = random.choice(self.start_points)
-        return self.text[start:finish].strip().upper()
+        a = random.choice(range(len(self.text)))
+        b = random.choice(range(len(self.text)))
+        while a == b:
+            b = random.choice(range(len(self.text)))
+
+        return self.text[min(a, b):max(a, b)].strip().upper()
 
     def run_test(self, trials: int) -> float:
         plaintexts = [self.pick_random_excerpt() for _ in range(trials)]
@@ -35,16 +33,23 @@ class CrackerTimer:
         finish = time.time()
 
         for i in range(trials):
-            assert decrypted[i][1] == keys[i]
-            assert decrypted[i][2] == plaintexts[i]
+            assert decrypted[i][2] == plaintexts[i], f"\n{decrypted[i][2]}\n{plaintexts[i]}"
+            assert self.keys_match(decrypted[i][1], keys[i]), f"\n{decrypted[i][1]}\n{keys[i]}"
 
         return finish - start
+
+    @staticmethod
+    def keys_match(k1: str, k2: str) -> bool:
+        if len(k1) >= len(k2):
+            return k1 == k2 * (len(k1) // len(k2))
+        else:
+            return k2 == k1 * (len(k2) // len(k1))
 
 
 def main():
     with open("rabbitsign_faq.txt") as f:
         text = f.read().strip()
-    CrackerTimer(text, VigenereCracker()).run_test(10)
+    CrackerTester(text, VigenereCracker(iterations=300)).run_test(8)
 
 
 if __name__ == '__main__':
