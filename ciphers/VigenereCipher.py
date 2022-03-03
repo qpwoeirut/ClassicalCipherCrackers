3ee58f842ea922714b2b1ec3275c0b8615464364
qpwoeirut <stanleyzhong8@gmail.com>
2022-03-03 15:49:03 -0800

    Fix whitespace

--- ciphers/VigenereCipher.py
+++ ciphers/VigenereCipher.py
@@ -7,7 +7,7 @@ from ciphers.Cipher import Cipher
 class VigenereCipher(Cipher):
     def __init__(self, key: str, alphabet: str = string.ascii_uppercase):
         super().__init__(key, alphabet=alphabet)
-        self.key_ords =[alphabet.index(k) for k in key]
+        self.key_ords = [alphabet.index(k) for k in key]
         self.indexes = {alphabet[i]: i for i in range(len(alphabet))}
 
     def encrypt(self, plaintext: str) -> str:
