1b0bac16ad2c3cb1ef4620d940d51027824da92a
qpwoeirut <stanleyzhong8@gmail.com>
2022-03-19 15:16:26 -0700

    Add SubstitutionCracker

---
+++ crackers/SubstitutionCracker.py
@@ -0,0 +1,28 @@
+import random
+import string
+from typing import Type
+
+from ciphers.Cipher import Cipher
+from crackers.ClimbingCracker import ClimbingCracker
+
+
+class SubstitutionCracker(ClimbingCracker):
+    def __init__(self, cipher: Type[Cipher], alphabet=string.ascii_uppercase, restart_threshold=200, iterations=5000):
+        super().__init__(cipher, alphabet=alphabet, restart_threshold=restart_threshold, iterations=iterations)
+
+    # TODO maybe make keys lists or tuples instead?
+    def generate_random_key(self) -> str:
+        letters = self.alphabet
+        random.shuffle(list(letters))
+        return ''.join(letters)
+
+    def mutate_key(self, key: str) -> str:
+        i1 = random.randint(0, len(key) - 1)
+        i2 = random.randint(0, len(key) - 1)
+        while i1 == i2:
+            i2 = random.randint(0, len(key) - 1)
+
+        if i1 > i2:
+            i1, i2 = i2, i1
+
+        return key[:i1] + key[i2] + key[i1 + 1:i2] + key[i1] + key[i2 + 1:]
