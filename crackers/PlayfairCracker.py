59021fb2c8d729ea0fe0df18b9101aab5e4e77e9
qpwoeirut <stanleyzhong8@gmail.com>
2023-10-27 23:30:00 -0700

    Adjust PlayfairCracker default parameters

--- crackers/PlayfairCracker.py
+++ crackers/PlayfairCracker.py
@@ -7,7 +7,7 @@ from crackers.ClimbingCracker import ClimbingCracker
 
 class PlayfairCracker(ClimbingCracker):
     # Alphabet is a required parameter since the default ascii_uppercase doesn't work
-    def __init__(self, alphabet: str, restart_threshold=500, iterations=800):
+    def __init__(self, alphabet: str, restart_threshold=800, iterations=600):
         super().__init__(restart_threshold=restart_threshold, iterations=iterations)
         self.alphabet = alphabet
         assert int(sqrt(len(self.alphabet)))**2 == len(self.alphabet), "alphabet length should be a perfect square"
@@ -25,4 +25,9 @@ class PlayfairCracker(ClimbingCracker):
         random_index1 = random.randint(0, len(key) - 1)
         new_key = list(key)
         new_key[random_index0], new_key[random_index1] = new_key[random_index1], new_key[random_index0]
-        return ''.join(new_key)
+
+        key = ''.join(new_key)
+
+        if random.randint(1, 4) == 1:  # have chance of multiple mutations
+            return self.mutate_key(key)
+        return key
